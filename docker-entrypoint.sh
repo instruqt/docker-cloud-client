5b25d87205f19f90231638a47832f58aa5b7874c
Chad Petersen <chad@instruqt.com>
2024-08-30 14:39:59 +0200

    Add warning fixes to docker entrypoint (#18)
    * Add warning fixes to docker entrypoint
    
    * Use retries variable in for loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,7 +19,7 @@
 
 gcloud_init() {
     if [ -n "${INSTRUQT_GCP_PROJECTS}" ]; then
-        PROJECTS=("${INSTRUQT_GCP_PROJECTS//,/ }")
+        IFS=',' read -r -a PROJECTS <<< "$INSTRUQT_GCP_PROJECTS"
 
         # load all credentials into gcloud
         for PROJECT in "${PROJECTS[@]}"; do
@@ -28,18 +28,18 @@ gcloud_init() {
             base64 -d <(echo "${!SERVICE_ACCOUNT_KEY}") > "$TMP_FILE"
 
             # Retry gcloud auth activate service account.
-            for (( i = 1, retries = 5; i <= 5; i++ )); do
-              if gcloud auth activate-service-account --key-file="$TMP_FILE"; then
-                  echo "Command succeeded on attempt $i."
-                  break
-              else
-                  echo "Command failed on attempt $i."
-                  # If it's not the last attempt, wait before retrying.
-                  if [ "$i" -lt "$retries" ]; then
-                      echo "Retrying in $i seconds..."
-                      sleep "$i"
-                  fi
-              fi
+            for (( i = 1, retries = 5; i <= retries; i++ )); do
+                if gcloud auth activate-service-account --key-file="$TMP_FILE"; then
+                    echo "Command succeeded on attempt $i."
+                    break
+                else
+                    echo "Command failed on attempt $i."
+                    # If it's not the last attempt, wait before retrying.
+                    if [ "$i" -lt "$retries" ]; then
+                        echo "Retrying in $i seconds..."
+                        sleep "$i"
+                    fi
+                fi
             done
 
             rm "$TMP_FILE"
@@ -47,44 +47,44 @@ gcloud_init() {
 
         # activate service account for first project
         SERVICE_ACCOUNT_EMAIL="INSTRUQT_GCP_PROJECT_${PROJECTS[0]}_SERVICE_ACCOUNT_EMAIL"
-        gcloud config set account "${!SERVICE_ACCOUNT_EMAIL}"
+        gcloud config set account "${!SERVICE_ACCOUNT_EMAIL}" --quiet
 
         # configure project
         PROJECT_ID="INSTRUQT_GCP_PROJECT_${PROJECTS[0]}_PROJECT_ID"
-        gcloud config set project "${!PROJECT_ID}"
+        gcloud config set project "${!PROJECT_ID}" --quiet
     fi
 }
 
 aws_init() {
     if [[ -n ${INSTRUQT_AWS_ACCOUNTS} ]]; then
-        PROJECTS=("${INSTRUQT_AWS_ACCOUNTS//,/ }")
+        IFS=',' read -r -a PROJECTS <<< "$INSTRUQT_AWS_ACCOUNTS"
 
         # load all credentials into aws configure
-        for PROJECT in ${PROJECTS[@]}; do
-		aws configure --profile $PROJECT  set region eu-west-1
-		[[ $PROJECT == ${PROJECTS[0]} ]] && aws configure --profile default set region eu-west-1
-		VAR="INSTRUQT_AWS_ACCOUNT_${PROJECT}_AWS_ACCESS_KEY_ID"
-		aws configure --profile $PROJECT  set aws_access_key_id "${!VAR}"
-		[[ $PROJECT == ${PROJECTS[0]} ]] && aws configure --profile default set aws_access_key_id "${!VAR}"
-		VAR="INSTRUQT_AWS_ACCOUNT_${PROJECT}_AWS_SECRET_ACCESS_KEY"
-		aws configure --profile $PROJECT  set aws_secret_access_key "${!VAR}"
-		[[ $PROJECT == ${PROJECTS[0]} ]] && aws configure --profile default set aws_secret_access_key "${!VAR}"
-		VAR="INSTRUQT_AWS_ACCOUNT_${PROJECT}_USERNAME"
-		USERNAME="${!VAR}"
+        for PROJECT in "${PROJECTS[@]}"; do
+            aws configure --profile "$PROJECT"  set region eu-west-1
+            [[ "$PROJECT" == "${PROJECTS[0]}" ]] && aws configure --profile default set region eu-west-1
+            VAR="INSTRUQT_AWS_ACCOUNT_${PROJECT}_AWS_ACCESS_KEY_ID"
+            aws configure --profile "$PROJECT"  set aws_access_key_id "${!VAR}"
+            [[ "$PROJECT" == "${PROJECTS[0]}" ]] && aws configure --profile default set aws_access_key_id "${!VAR}"
+            VAR="INSTRUQT_AWS_ACCOUNT_${PROJECT}_AWS_SECRET_ACCESS_KEY"
+            aws configure --profile "$PROJECT"  set aws_secret_access_key "${!VAR}"
+            [[ "$PROJECT" == "${PROJECTS[0]}" ]] && aws configure --profile default set aws_secret_access_key "${!VAR}"
+            VAR="INSTRUQT_AWS_ACCOUNT_${PROJECT}_USERNAME"
+            USERNAME="${!VAR}"
         done
     fi
 }
 
 azure_init() {
     if [[ -n $INSTRUQT_AZURE_SUBSCRIPTIONS ]]; then
-        SUBSCRIPTIONS=("${INSTRUQT_AZURE_SUBSCRIPTIONS//,/ }")
+        IFS=',' read -r -a SUBSCRIPTIONS <<< "$INSTRUQT_AZURE_SUBSCRIPTIONS"
 
         source /etc/bash_completion.d/azure-cli
         USERNAME="INSTRUQT_AZURE_SUBSCRIPTION_${SUBSCRIPTIONS[0]}_USERNAME"
         PASSWORD="INSTRUQT_AZURE_SUBSCRIPTION_${SUBSCRIPTIONS[0]}_PASSWORD"
         az login --username="${!USERNAME}" --password="${!PASSWORD}"
 
-        mkdir -p "$HOME/.azure/credentials"
+        mkdir -p "$HOME/.azure"
         cat <<EOF > "$HOME/.azure/credentials"
 [$ARM_SUBSCRIPTION_ID]
 client_id=$ARM_CLIENT_ID
